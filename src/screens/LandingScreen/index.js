54b1af0d75e95abcfe654609fe071d19d271f461
Matthew Smith <azamatsmith@gmail.com>
2018-02-08 07:09:02 -0700

    Run prettier

--- src/screens/LandingScreen/index.js
+++ src/screens/LandingScreen/index.js
@@ -1,6 +1,6 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import {StyleSheet, Text, View} from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default class LandingScreen extends Component {
   static propTypes = {};
