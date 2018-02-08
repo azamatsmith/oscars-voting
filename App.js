6792e564ca70063c7c7e0e69ddccd843a2b587c1
Matthew Smith <azamatsmith@gmail.com>
2018-02-08 06:49:16 -0700

    Add env.js

--- App.js
+++ App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import {AppRegistry} from 'react-native';
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import env from './env.js';
 
 import rootReducer from './src/reducers';
 import AppWithNavigationState from './src/navigators';
-import { middleware } from './src/utils/redux';
+import {middleware} from './src/utils/redux';
 
-const store = createStore(
-  rootReducer,
-  applyMiddleware(middleware),
-);
+// Set Env variables
+env();
+
+const store = createStore(rootReducer, applyMiddleware(middleware));
 
 class App extends React.Component {
   render() {
@@ -23,4 +24,3 @@ class App extends React.Component {
 }
 
 export default App;
-
---
+++ env.js
@@ -0,0 +1,5 @@
+module.exports = () => {
+  // ADD ENV VARS HERE
+  //process.env.SOME_API_KEY = 'SECRET';
+  process.env.OMDB_API_KEY = 'c133e5dc';
+};
