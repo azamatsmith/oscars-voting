baf18c87d745df9098e1dc11ea7037f8c484ed90
sethkfman <sethkfman@gmail.com>
2018-02-07 20:48:16 -0700

    Added firebase init to project

--- App.js
+++ App.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { AppRegistry, BackHandler } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
+import firebase from 'firebase';
 
 import rootReducer from './src/reducers';
 import AppWithNavigationState from './src/navigators';
@@ -19,6 +20,24 @@ class App extends React.Component {
         <AppWithNavigationState />
       </Provider>
     );
+  }
+
+  componentDidMount() {
+    BackHandler.addEventListener('hardwareBackPress', this._onBackPress);
+    // make sure not to initialize firebase apps more than once
+    if (firebase.apps.length > 0) {
+      return;
+    }
+    
+    const config = {
+      apiKey: process.env.API_KEY,
+      authDomain: 'oscars-29f50.firebaseapp.com',
+      databaseURL: 'https://oscars-29f50.firebaseio.com',
+      projectId: 'oscars-29f50',
+      storageBucket: '',
+      messagingSenderId: '695218268092',
+    };
+    firebase.initializeApp(config);
   }
 }
 
