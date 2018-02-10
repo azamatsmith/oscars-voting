cc61290e86b14545530987b513653a4ea2b415f7
Matthew Smith <azamatsmith@gmail.com>
2018-02-10 12:58:57 -0700

    Write test for <LandingScreen />

--- src/screens/LandingScreen/index.js
+++ src/screens/LandingScreen/index.js
@@ -1,10 +1,15 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {categories} from 'src/utils/data';
 
 export default class LandingScreen extends Component {
   static propTypes = {};
   static defaultProps = {};
+
+  // PRIVATE
+
+  _renderList = () => null;
 
   render() {
     return (
