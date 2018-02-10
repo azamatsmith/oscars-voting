a8047659a136e79332fd0a46e0b3de8305b8e03e
Matthew Smith <azamatsmith@gmail.com>
2018-02-10 16:02:59 -0700

    Add CategoryDataReducer

---
+++ src/reducers/CategoryDataReducer.js
@@ -0,0 +1,14 @@
+const initialState = {
+  categoryData: [],
+  loading: false,
+  selectedCategory: null,
+};
+
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case '':
+      return state;
+    default:
+      return state;
+  }
+};
