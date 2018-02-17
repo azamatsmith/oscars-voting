af280419b1888887381004172c6781eae2b64bea
Matthew Smith <azamatsmith@users.noreply.github.com>
2018-02-17 15:28:07 -0700

    Feature - Add "Best Animated Feature" (#50)
    * Update data.js - add ids for animated feature
    
    * Update <CategoryScreen /> tests

--- src/utils/data.js
+++ src/utils/data.js
@@ -17,7 +17,17 @@ export const categories = [
   { id: 1, category: 'Actor in a Leading Role' },
   { id: 2, category: 'Actress in a Leading Role' },
   { id: 3, category: 'Actor in a Supporting Role' },
-  { id: 4, category: 'Animated Feature Film' },
+  {
+    id: 4,
+    category: 'Animated Feature Film',
+    items: [
+      'tt3874544', // Boss baby
+      'tt3874544', // breadwinniner
+      'tt2380307', // coco
+      'tt3411444', // ferdinand
+      'tt3262342', // loving vincent
+    ],
+  },
   { id: 5, category: 'Cinematography' },
   { id: 6, category: 'Costume Design' },
   { id: 7, category: 'Directing' },
