6fe09285d2458ac728637112ce4ea0960b0aff1d
Matthew Smith <azamatsmith@gmail.com>
2018-02-17 15:23:09 -0700

    Update <CategoryScreen /> tests

--- src/screens/CategoryScreen/CategoryScreen.test.js
+++ src/screens/CategoryScreen/CategoryScreen.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import { CategoryScreen } from './';
+import { categories } from 'src/utils/data';
 
 describe('<CategoryScreen />', () => {
   const props = {
-    categoryData: [],
+    categoryData: categories[0],
     fetchCategoryData: jest.fn(),
     navigation: {
       state: {
@@ -20,5 +21,13 @@ describe('<CategoryScreen />', () => {
 
   it('renders without crashing', () => {
     expect(shallowWrapper.find('MainView').length).toEqual(1);
+  });
+
+  it('has a function that increments and decrements the state', () => {
+    expect(shallowWrapper.state().selectedItemIndex).toEqual(0);
+    shallowWrapper.instance()._handleCycle(1);
+    expect(shallowWrapper.state().selectedItemIndex).toEqual(1);
+    shallowWrapper.instance()._handleCycle(-1);
+    expect(shallowWrapper.state().selectedItemIndex).toEqual(0);
   });
 });
