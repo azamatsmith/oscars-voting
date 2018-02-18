51fe9185460ed5631e5313d1f00d5e97d28020a1
Matthew Smith <azamatsmith@gmail.com>
2018-02-17 17:42:38 -0700

    Add CategoryDataActions tests

--- src/actions/CategoryDataActions.js
+++ src/actions/CategoryDataActions.js
@@ -1,14 +1,14 @@
 import axios from 'axios';
-import { categories } from 'src/utils/data';
+import {categories} from 'src/utils/data';
 import firebase from 'firebase';
 
 // MTS - id corresponds with the index of each category
 export const fetchCategoryData = id => dispatch => {
   // clear out data from previous category
-  dispatch({ type: 'CLEAR_DATA' });
+  dispatch({type: 'CLEAR_DATA'});
 
   // items is an array of ids
-  const { items } = categories[id];
+  const {items} = categories[id];
 
   firebase
     .database()
@@ -21,6 +21,8 @@ export const fetchCategoryData = id => dispatch => {
         id: key,
       }));
       const filteredData = data.filter(obj => items.indexOf(obj.id) >= 0);
-      dispatch({ type: 'UPDATE_CATEGORY_DATA', categoryData: filteredData });
-    });
+      dispatch({type: 'UPDATE_CATEGORY_DATA', categoryData: filteredData});
+    })
+    // eslint-disable-next-line
+    .catch(err => console.log('error fetching category data', err));
 };
--- src/utils/setupTests.js
+++ src/utils/setupTests.js
@@ -1,6 +1,20 @@
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+import firebase from 'firebase';
 
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({adapter: new Adapter()});
 
 global.shalllow = Enzyme.shallow;
+
+// Start firebase
+
+const config = {
+  apiKey: '',
+  authDomain: '',
+  databaseURL: 'https://oscars-29f50.firebaseio.com',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+};
+
+firebase.initializeApp(config);
