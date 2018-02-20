ef281eaa5af893ffaef354768f8969660d0d592e
sethkfman <sethkfman@gmail.com>
2018-02-19 21:46:35 -0700

    formatting

--- src/screens/CreateGroupScreen/index.js
+++ src/screens/CreateGroupScreen/index.js
@@ -5,8 +5,7 @@ import { MainView, Text, Button, Input } from 'src/common';
 import firebase from 'firebase';
 
 export default class CreateGroupScreen extends Component {
-
-  state = {userName: '', groupName: ''}
+  state = { userName: '', groupName: '' };
 
   static navigationOptions = {
     title: 'Oscars Voting - 2018',
@@ -15,71 +14,70 @@ export default class CreateGroupScreen extends Component {
   static propTypes = {
     navigation: PropTypes.shape({
       navigate: PropTypes.func.isRequired,
-    })
+    }),
   };
 
-  static defaultProps = {  };
+  static defaultProps = {};
 
   // PRIVATE
 
-    //Method for firebase add group
-    _createGroup = () => {
-      
-      var dbRef = firebase.database();
+  //Method for firebase add group
+  _createGroup = () => {
+    var dbRef = firebase.database();
 
-      //Get unique key for group
-      var groupKey = dbRef
-        .ref()
-        .child('groups')
-        .push().key;
+    //Get unique key for group
+    var groupKey = dbRef
+      .ref()
+      .child('groups')
+      .push().key;
 
-      //Create Group
-      dbRef.ref('groups/' + groupKey).set({
-        name: this.state.groupName,
-        groupId: this._randomGroupId()
-      });
+    //Create Group
+    dbRef.ref('groups/' + groupKey).set({
+      name: this.state.groupName,
+      groupId: this._randomGroupId(),
+    });
 
-      //Create User
-      dbRef.ref('users/' + this.state.userName).set({
-        name: this.state.userName,
-        admin: true,
-        groupId: groupKey
-      });
-    };
-    
-    _randomGroupId = () => {return Math.floor(1000 + Math.random() * 9000)};
+    //Create User
+    dbRef.ref('users/' + this.state.userName).set({
+      name: this.state.userName,
+      admin: true,
+      groupId: groupKey,
+    });
+  };
 
+  _randomGroupId = () => {
+    return Math.floor(1000 + Math.random() * 9000);
+  };
 
-    render() {
-      return (
-        <MainView>
-          <View style={styles.groupViewStyle}>
-            <Text> Create a new group. </Text>
+  render() {
+    return (
+      <MainView>
+        <View style={styles.groupViewStyle}>
+          <Text> Create a new group. </Text>
 
-            <Input 
-              label='Group Name'
-              onChangeText={groupName => this.setState({ groupName })}
-              placeholder="groupname"
-              value={this.state.groupName}
-            />
+          <Input
+            label="Group Name"
+            onChangeText={groupName => this.setState({ groupName })}
+            placeholder="groupname"
+            value={this.state.groupName}
+          />
 
-            <Input 
-              label="User Name"
-              onChangeText={userName => this.setState({ userName })}
-              placeholder="username"
-              value={this.state.userName}            
-            /> 
+          <Input
+            label="User Name"
+            onChangeText={userName => this.setState({ userName })}
+            placeholder="username"
+            value={this.state.userName}
+          />
 
-            <Button
-              onPress={() => this._createGroup()}
-              style={styles.buttonStyle}
-              text="Create Group"
-            />
-            
-          </View>
-        </MainView>
-      );
-    }
+          <Button
+            onPress={() => this._createGroup()}
+            style={styles.buttonStyle}
+            text="Create Group"
+          />
+        </View>
+      </MainView>
+    );
+  }
 }
 
 // Export class so that you do not have to mount redux store in tests
@@ -87,7 +85,7 @@ export { CreateGroupScreen };
 
 const styles = StyleSheet.create({
   groupViewStyle: {
-    flex: .25,
+    flex: 0.25,
     flexDirection: 'column',
     justifyContent: 'center',
     alignItems: 'center',
