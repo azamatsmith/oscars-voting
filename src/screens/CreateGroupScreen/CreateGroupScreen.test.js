8f317ee09d9da4b0c2ddc3397ce5876d65f4c87e
sethkfman <sethkfman@gmail.com>
2018-02-22 20:54:30 -0700

    updated test case

--- src/screens/CreateGroupScreen/CreateGroupScreen.test.js
+++ src/screens/CreateGroupScreen/CreateGroupScreen.test.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import CreateGroupScreen from './';
-
+import { shallow } from 'enzyme';
 import renderer from 'react-test-renderer';
 
+
 describe('<CreateGroupScreen />', () => {
+  const props = {
+    navigation: {
+      navigate: jest.fn(),
+    },
+  };
+
+  const shallowWrapper = shallow(<CreateGroupScreen {...props} />);
+
   it('renders without crashing', () => {
     const rendered = renderer.create(<CreateGroupScreen />).toJSON();
     expect(rendered).toBeTruthy();
   });
 
   it('has a function that generates a random number', () => {
-    const groupId = rendered.instance()._randomGroupId();
+    const groupId = shallowWrapper.instance()._randomGroupId();
     expect(groupId).toBeGreaterThan(0);
   });
 
